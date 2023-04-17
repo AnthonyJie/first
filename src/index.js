a35e16ddf205445796ab831d674f274a1742d89b
jackson <834929162@qq.com>
2023-04-17 19:29:22 +0800

    函数声明组件

--- src/index.js
+++ src/index.js
@@ -1,6 +1,30 @@
 import 'normalize.css'
 import ReactDom from 'react-dom'
 
-const element = <strong>666</strong>
+// function Home() {
+//   return <h1>这是home组件</h1>
+// }
+
+const Home = () => <h1>这是home组件</h1>
+
+export const hooks = () => {
+  let count = 1
+  const addCount = () => {
+    count += 1
+    console.log(count)
+  }
+
+  return { count, addCount }
+}
+
+const { addCount, count } = hooks()
+
+const element = (
+  <>
+    <Home></Home>
+    <span>{count}</span>
+    <button onClick={addCount}>加加</button>
+  </>
+)
 
 ReactDom.render(element, document.querySelector('#root'))
