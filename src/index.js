af9955f347e3687a5bcb4a409bfe73caaeaa9d87
jackson <834929162@qq.com>
2023-04-18 16:38:37 +0800

    class的state

--- src/index.js
+++ src/index.js
@@ -1,19 +1,28 @@
 import ReactDom from 'react-dom'
+
 import 'normalize.css'
+import { Component } from 'react'
 
-import Hello from './components/Hello'
-
-import Demo from './components/Demo'
-// 类组件必须继承component构造函数
-
-// 必须要写render方法 且一定要有返回值
+class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      count: 0,
+    }
+  }
+  render() {
+    return (
+      <div>
+        <h1>我是跟组件</h1>
+        <h2>count: {this.state.count}</h2>
+      </div>
+    )
+  }
+}
 
 const element = (
   <>
-    <div>
-      <Hello></Hello>
-      <Demo></Demo>
-    </div>
+    <App></App>
   </>
 )
 
