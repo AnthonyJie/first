5450bd7d9efec1f3c4ed972de82986b119297bf3
jackson <834929162@qq.com>
2023-04-19 16:27:08 +0800

    tab栏切换

--- src/index.js
+++ src/index.js
@@ -1,33 +1,63 @@
 import 'normalize.css'
 import { Component } from 'react'
-import ReactDom from 'react-dom'
 
-class App extends Component {
+import { createRoot } from 'react-dom/client'
+
+import './index.css'
+
+let active = 0
+
+class Person extends Component {
   constructor() {
     super()
     this.state = {
-      count: 0,
+      msg: 0,
     }
+    this.arr = [
+      { id: 1, name: '推荐' },
+      { id: 2, name: '一人套餐' },
+      { id: 3, name: '西贝凉菜' },
+      { id: 4, name: '西贝热菜' },
+      { id: 5, name: '杂粮主食' },
+    ]
   }
 
   render() {
     return (
       <>
-        <h1>{this.state.count}</h1>
-        <button onClick={() => this.addCount()}>+1</button>
+        <div>{this.state.msg}</div>
+        <button onClick={this.addCount}>访问msg</button>
+        <ul>
+          {this.arr.map((item, index) => (
+            <li
+              onClick={() => this.handleClick(index)}
+              className={`${index === 0 ? 'first' : ''} ${
+                active === index ? 'active' : ''
+              } children`}
+              key={item.id}
+            >
+              {item.name}
+            </li>
+          ))}
+        </ul>
       </>
     )
   }
+  handleClick(i) {
+    active = i
+    console.log(i, active)
+    const before = document.querySelector('.active')
+    before.classList.remove('active')
+    const selection = document.querySelectorAll('.children')[i]
+    selection.classList.add('active')
+  }
 
-  addCount() {
-    console.log(this.state.count)
+  addCount = () => {
+    // this.state.msg += 1
+    console.log(this.state.msg)
   }
 }
 
-// const element = (
-//   <>
-//     <App></App>
-//   </>
-// )
+const root = createRoot(document.querySelector('#root'))
 
-ReactDom.render(<App></App>, document.querySelector('#root'))
+root.render(<Person></Person>)
